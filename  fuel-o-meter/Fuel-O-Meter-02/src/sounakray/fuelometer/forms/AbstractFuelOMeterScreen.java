6e3b7f00c6f239d715f892b909e62cc9004f0f13
sounakray <sounakray@972e18fa-86f7-11de-9934-f1fc26e2a4cc>
2009-08-14 20:42:44 +0000

    Renamed the abstract form class. Added an Implicit List to the main menu.

---  fuel-o-meter/Fuel-O-Meter-02/src/sounakray/fuelometer/forms/AbstractFuelOMeterScreen.java
+++  fuel-o-meter/Fuel-O-Meter-02/src/sounakray/fuelometer/forms/AbstractFuelOMeterScreen.java
@@ -10,14 +10,14 @@ import sounakray.fuelometer.midlet.FuelOMeter;
 /**
  * @author Sounak Ray
  */
-public abstract class AbstractForm {
+public abstract class AbstractFuelOMeterScreen {
 	protected final FuelOMeter midlet;
 	protected final Displayable screen;
 
 	/**
 	 * @param arg0
 	 */
-	public AbstractForm(final Displayable screen, final FuelOMeter midlet) {
+	public AbstractFuelOMeterScreen(final Displayable screen, final FuelOMeter midlet) {
 		this.screen = screen;
 		this.midlet = midlet;
 		screen.setCommandListener(midlet);
