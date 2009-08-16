43e1318e4cb81c55c0a8fb203b669cff0784c67e
sounakray <sounakray@972e18fa-86f7-11de-9934-f1fc26e2a4cc>
2009-08-16 13:16:58 +0000

    IMPORTANT COMMIT!
    Implemented statistics calculations.
    Only basic - not detailed.

---
+++ trunk/ fuel-o-meter/Fuel-O-Meter-02/src/sounakray/fuelometer/model/StatisticsData.java
@@ -0,0 +1,195 @@
+/**
+ * StatisticsData.java
+ * Fuel-O-Meter-02
+ * @version %I%, %G%
+ * Date Aug 16, 2009
+ * © Sounak Ray
+ * email: sounakray@gmail.com
+ */
+package sounakray.fuelometer.model;
+
+/**
+ * @author Sounak Ray
+ */
+public final class StatisticsData {
+	private String startDate = "";
+	private String endDate = "";
+	private String averageMileage = "";
+	private String lastMileage = "";
+	private String totalDistance = "";
+	private String totalDays = "";
+	private String totalFuelConsumed = "";
+	private String totalMoneyConsumed = "";
+	private String totalFuel = "";
+	private String totalMoney = "";
+	private String fuelPer100Dist = "";
+	private String averageCostPerDist = "";
+
+	/**
+	 * @return the startDate
+	 */
+	public final String getStartDate(){
+		return this.startDate;
+	}
+
+	/**
+	 * @param startDate the startDate to set
+	 */
+	public final void setStartDate(String startDate){
+		this.startDate = startDate;
+	}
+
+	/**
+	 * @return the endDate
+	 */
+	public final String getEndDate(){
+		return this.endDate;
+	}
+
+	/**
+	 * @param endDate the endDate to set
+	 */
+	public final void setEndDate(String endDate){
+		this.endDate = endDate;
+	}
+
+	/**
+	 * @return the averageMileage
+	 */
+	public final String getAverageMileage(){
+		return this.averageMileage;
+	}
+
+	/**
+	 * @param averageMileage the averageMileage to set
+	 */
+	public final void setAverageMileage(String averageMileage){
+		this.averageMileage = averageMileage;
+	}
+
+	/**
+	 * @return the lastMileage
+	 */
+	public final String getLastMileage(){
+		return this.lastMileage;
+	}
+
+	/**
+	 * @param lastMileage the lastMileage to set
+	 */
+	public final void setLastMileage(String lastMileage){
+		this.lastMileage = lastMileage;
+	}
+
+	/**
+	 * @return the totalDistance
+	 */
+	public final String getTotalDistance(){
+		return this.totalDistance;
+	}
+
+	/**
+	 * @param totalDistance the totalDistance to set
+	 */
+	public final void setTotalDistance(String totalDistance){
+		this.totalDistance = totalDistance;
+	}
+
+	/**
+	 * @return the totalDays
+	 */
+	public final String getTotalDays(){
+		return this.totalDays;
+	}
+
+	/**
+	 * @param totalDays the totalDays to set
+	 */
+	public final void setTotalDays(String totalDays){
+		this.totalDays = totalDays;
+	}
+
+	/**
+	 * @return the totalFuelConsumed
+	 */
+	public final String getTotalFuelConsumed(){
+		return this.totalFuelConsumed;
+	}
+
+	/**
+	 * @param totalFuelConsumed the totalFuelConsumed to set
+	 */
+	public final void setTotalFuelConsumed(String totalFuelConsumed){
+		this.totalFuelConsumed = totalFuelConsumed;
+	}
+
+	/**
+	 * @return the totalMoneyConsumed
+	 */
+	public final String getTotalMoneyConsumed(){
+		return this.totalMoneyConsumed;
+	}
+
+	/**
+	 * @param totalMoneyConsumed the totalMoneyConsumed to set
+	 */
+	public final void setTotalMoneyConsumed(String totalMoneyConsumed){
+		this.totalMoneyConsumed = totalMoneyConsumed;
+	}
+
+	/**
+	 * @return the totalFuel
+	 */
+	public final String getTotalFuel(){
+		return this.totalFuel;
+	}
+
+	/**
+	 * @param totalFuel the totalFuel to set
+	 */
+	public final void setTotalFuel(String totalFuel){
+		this.totalFuel = totalFuel;
+	}
+
+	/**
+	 * @return the totalMoney
+	 */
+	public final String getTotalMoney(){
+		return this.totalMoney;
+	}
+
+	/**
+	 * @param totalMoney the totalMoney to set
+	 */
+	public final void setTotalMoney(String totalMoney){
+		this.totalMoney = totalMoney;
+	}
+
+	/**
+	 * @return the fuelPer100Dist
+	 */
+	public final String getFuelPer100Dist(){
+		return this.fuelPer100Dist;
+	}
+
+	/**
+	 * @param fuelPer100Dist the fuelPer100Dist to set
+	 */
+	public final void setFuelPer100Dist(String fuelPer100Dist){
+		this.fuelPer100Dist = fuelPer100Dist;
+	}
+
+	/**
+	 * @return the averageCostPerDist
+	 */
+	public final String getAverageCostPerDist(){
+		return this.averageCostPerDist;
+	}
+
+	/**
+	 * @param averageCostPerDist the averageCostPerDist to set
+	 */
+	public final void setAverageCostPerDist(String averageCostPerDist){
+		this.averageCostPerDist = averageCostPerDist;
+	}
+}
