700dbcd2498c5a1171b258a419d5b92b822fc655
sounakray <sounakray@972e18fa-86f7-11de-9934-f1fc26e2a4cc>
2009-08-16 20:03:16 +0000

    Importance of Commit   : HIGH Reason for update      : Implemented data caching in the DAO implementation layer. Features Updated/Added : Caching, Javadocs Committed By           : Sounak Ray

--- trunk/ fuel-o-meter/Fuel-O-Meter-02/src/sounakray/fuelometer/DAO/FuelOMeterDAO.java
+++ trunk/ fuel-o-meter/Fuel-O-Meter-02/src/sounakray/fuelometer/DAO/FuelOMeterDAO.java
@@ -11,14 +11,27 @@ package sounakray.fuelometer.DAO;
 import sounakray.fuelometer.model.FillUp;
 
 /**
+ * Interface Description: The DAO Interface for the FuelOMeter MIDlet application. <br>
+ * The possible implementations of this interface may include RecordStore, File Connections etc, or even a remote cloud
+ * storage.
  * @author Sounak Ray
+ * @since Aug 16, 2009
  */
 public interface FuelOMeterDAO {
-	boolean openDataStore();
-
-	boolean closeDataStore();
-
+	/**
+	 * Method Description: This method should save the FillUp bean passed.
+	 * @param fillUp The pre-populated bean to be saved.
+	 * @return true if the operation was carried out successfully; false otherwise.
+	 * @author Sounak Ray
+	 * @since Aug 16, 2009
+	 */
 	boolean saveFillUp(final FillUp fillUp);
 
-	FillUp[] getFillUpList();
+	/**
+	 * Method Description: This method should fetch an array of all the FillUp records.
+	 * @return array of all the FillUp records.
+	 * @author Sounak Ray
+	 * @since Aug 16, 2009
+	 */
+	FillUp[] getAllFillUpList();
 }
